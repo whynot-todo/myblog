d7ef801fda53f0e4fa9e1c749476a3baa6c213cd
Shengfu Cai <2211986234@qq.com>
2021-02-05 19:24:40 +0800

    'deploy'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,6 +11,16 @@ module.exports = {
     themeConfig: {
         nav: Nav,
         sidebar: 'auto',
+        // 最近更新
+        lastUpdated: 'Last Updated',
+        // 标题链接的深度
+        sidebarDepth: 2,
+        // 开启滚动
+        smoothScroll: true,
+        // 在github上编辑
+        repo: 'https://github.com/whynot-todo/myblog/',
+        repoLabel: '查看源码',
+        docsBranch: 'main',
         editLinks: true,
         editLinkText: '在 GitHub 上编辑此页 ！'
     }
