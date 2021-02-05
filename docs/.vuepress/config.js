e641c43a4bb3d9e86c0016c879e0e62b2b39bd71
Shengfu Cai <2211986234@qq.com>
2021-02-05 19:33:15 +0800

    'deploy'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const Nav = require('./nav.js')
 
 module.exports = {
     base: '/myblog/', /* 基础虚拟路径 */
-    dest: 'docs/dist', /* 打包文件基础路径, 在命令所在目录下 */
+    dest: 'dist', /* 打包文件基础路径, 在命令所在目录下 */
     title: '我的博客',
     description: '客官要点什么酒',
     markdown: {
